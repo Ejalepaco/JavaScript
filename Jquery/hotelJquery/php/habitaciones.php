d9367c4034b3e86bb7bcc2bff1deb5fa2837c79a
Ejalepaco <fdominguezp@gmail.com>
2025-06-22 21:48:20 +0200

    Revert "Fichero a Excel"
    This reverts commit d7caf47a2708d32ff2a284e0721d809f971f808f.

--- Jquery/hotelJquery/php/habitaciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'hotelJquery');
-$result = $conn->query("SELECT id, nombre FROM habitaciones");
-$habitaciones = [];
-while ($row = $result->fetch_assoc()) {
-    $habitaciones[] = $row;
-}
-echo json_encode($habitaciones);
-?>
